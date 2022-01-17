1400b2ca4b52eafc068367775356541ba8a2937b
Vamsi Karnatapu <karnatav@oregonstate.edu>
2022-01-16 19:26:22 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8080
+ 
+var apiMocker = require('connect-api-mocker');
+const resp = require("./response.json")
+app.get('/data/2.5/weather', (req, res) => {
+   console.log("hi")
+   res.send(resp);   
+})
+ 
+app.listen(port, () => {
+ console.log(`Example app listening at http://localhost:${port}`)
+})
